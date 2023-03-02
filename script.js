bec37c230b5217165f59f86f5977aec41c727341
WouterSp <126822332+WouterSp@users.noreply.github.com>
2023-03-02 22:05:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ else {
     handleCharacteristicValueChanged = (event) => {
         let value = event.target.value ; // a dataviewer object is provided by the object event
         let heartrate = value.getUint8(1) ; // we select the eight bytes that contain the heartrate informations
-        p.textContent = heartrate + " BPM" ; // and display it
+        p.textContent = heartrate + " bar" ; // and display it
         BPM = heartrate ;
     }
     onClickEvent = () => {
         navigator.bluetooth.requestDevice({ filters: [{ name: 'XC1004 BLE Server' }] }) // we filter the devices, displaying only those with heartrate services
         .then(device => device.gatt.connect()) // after the user select a device, we return the selected one
-        .then(server => server.getPrimaryService('d7b7acd1-6785-4119-b156-c54B498BD873')) // we get the service
+        .then(server => server.getPrimaryService('d7b7acd1-6785-4119-b156-c54b498bd873')) // we get the service
         .then(service => service.getCharacteristic('3ef1c3a0-9f97-49B2-b3E4-78cfad8a3763')) // then the characteristics
         .then(characteristic => characteristic.startNotifications())
         .then(characteristic => {          
@@ -25,42 +25,6 @@ else {
         .catch(error => { console.error(error); }) ; // we display the errors on the console
     }
     button.addEventListener('click', onClickEvent ) ;
-    let startTime = performance.now() ;
-    let step = 0 ;
-    updateHeartSize = () => {
-        if (BPM > 0)
-        {
-            let ibi = 60./BPM * 1000 ;
-            let elapsedTime = performance.now() - startTime ;
-            let scaleUp = 1.1 ;
-            let scaleDown = (1/scaleUp).toFixed(2) ;
-            if (elapsedTime < ibi * 0.05 && step == 0 )
-            {
-                step++ ;
-                heartSprite.style.transform = "scale(" + scaleUp + ")" ;
-            }
-            else if ( elapsedTime > ibi * 0.05 && elapsedTime < ibi * 0.22 && step == 1)
-            {
-                step++ ;
-                heartSprite.style.transform = "scale(" + scaleDown + ")" ;
-            }
-            else if ( elapsedTime > ibi * 0.22 && elapsedTime < ibi * 0.26 && step == 2)
-            {
-                step++ ;
-                heartSprite.style.transform = "scale(" + scaleUp + ")" ;
-            }
-            else if (elapsedTime > ibi * 0.26 && step == 3)
-            {
-                step++ ;
-                heartSprite.style.transform = "scale(" + scaleDown + ")" ;
-            }
-            if (elapsedTime > ibi)
-            {
-                step = 0 ;
-                startTime = performance.now() ;
-            }
-        }
-        globalID = requestAnimationFrame(updateHeartSize) ;
-    } ;
-    let globalID = requestAnimationFrame(updateHeartSize) ;
+   
+         
 }
